3689ba0f1bad456f6ca76bbf1cbd8de9ea5ad9c6
angeEmmanuel002 <287430936+angeEmmanuel002@users.noreply.github.com>
2026-06-18 18:07:36 +0000

    règlement de la discordance entre le HTML serveur et celui client

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} font-sans antialiased`}
       >
