92d640851fb52fef331dbb2741eea53d93c09c8e
angeEmmanuel002 <287430936+angeEmmanuel002@users.noreply.github.com>
2026-06-21 11:27:37 +0000

    gestion de l'accès aux pages en fonction du statut d'enregistrement

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,6 +1,10 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
+//import de prisma pour récupérer des données en base
+import { prisma } from '@/lib/prisma'
+import { AwardIcon } from 'lucide-react'
+
 
 // Sumulation d'un utilisateur connecté en développement pour tests locaux
 const IS_DEV = process.env.NODE_ENV === 'development'
@@ -81,6 +85,23 @@ export async function updateSession(request: NextRequest) {
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
     return NextResponse.redirect(url)
+  }
+  if (user && !isPublicPath){
+    // recupération du statut d'enregistrement de l'utilisateur
+    const member = await prisma.member.findUnique({
+      where : {id : user.id},
+      select : { registration_status: true},
+    })
+    if (member?.registration_status === 'PENDING' && pathname !== '/auth/pending-review'){
+      const url = request.nextUrl.clone()
+      url.pathname = '/auth/pending-review'
+      return NextResponse.redirect(url)
+    }
+    if (member?.registration_status === 'REJECTED' && pathname !== '/auth/registration-rejected'){
+      const url = request.nextUrl.clone()
+      url.pathname = '/auth/registration-rejected'
+      return NextResponse.redirect(url)
+    }
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
